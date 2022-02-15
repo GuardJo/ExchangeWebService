17bcc37e840f0f885d0328aa07b70dbfdb67a2f3
LAPTOP-EDTDSUH9\dagh1 <dagh1218@gmail.com>
2022-02-16 02:21:28 +0900

    프로젝트 세팅 및 기초 view 생성

---
+++ demo/src/main/java/com/wire/test/config/MvcConfig.java
@@ -0,0 +1,34 @@
+package com.wire.test.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/view/", ".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+        messageSource.setBasename("messages.label");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+}
